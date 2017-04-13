947f315a53aaef61cf97dec7e2502ab880f9fe64
Diego Leonardo Urban <diego.urban88@gmail.com>
2017-04-13 18:31:03 -0300

    edit

--- client/app/cadastro/cadastro.component.ts
+++ client/app/cadastro/cadastro.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FotoComponent } from '../foto/foto.component';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { FotoService } from '../foto/foto.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
     moduleId: module.id,
@@ -13,9 +14,26 @@ export class CadastroComponent {
     foto: FotoComponent = new FotoComponent();
     meuForm: FormGroup;
     service: FotoService;
+    route: ActivatedRoute;
+    router: Router;
 
-    constructor(service: FotoService, fb: FormBuilder) {
+    constructor(service: FotoService, fb: FormBuilder, route: ActivatedRoute, router: Router) {
         this.service = service;
+        this.route = route;
+        this.router = router;
+        
+        this.route.params.subscribe(params => {
+          let id = params['id'];
+          if(id) {
+            this.service
+                .buscaPorId(id)
+                .subscribe(
+                    foto => this.foto = foto, 
+                    err => console.log(err)
+                );
+          }
+        });
+
         this.meuForm = fb.group({
             titulo: ['', Validators.compose([Validators.required, Validators.minLength(4)])],
             url: ['', Validators.required],
